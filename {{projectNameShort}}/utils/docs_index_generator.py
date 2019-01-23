43ec815c58440fc4b84472f102e22d9219edd43b
Alexandr Olferuk <olferuk@surfstudio.ru>
2019-01-23 16:40:43 +0300

    Automatic docs generation via makefile

---
+++ {{projectNameShort}}/utils/docs_index_generator.py
@@ -0,0 +1,51 @@
+from jinja2 import Template
+from toolz import groupby
+
+import glob
+import janus
+import os
+import re
+
+
+def make_doc_index(template_path: str, doc_index_path: str):
+    with open(template_path) as f:
+        template = Template(f.read())
+
+    try:
+        project_name = re.findall(r'name\s*\=\s*[\'\"](.+)[\"\']', open('../setup.py').read())[0]
+    except:
+        project_name = 'Unnamed'
+    project_name = '{0}\n{1}'.format(project_name, '*'*len(project_name))
+
+    fs = [x for x in glob.glob('../src/**', recursive=True)
+          if ('__pycache__' not in x) and ('__init__' not in x)]
+    fs = [x for x in fs if not os.path.isdir(x)]
+    fg = groupby(key=lambda s: os.path.dirname(s), seq=fs)
+
+    S = ''
+    for k in sorted(fg.keys()):
+        s = 'Module {}'.format(k[3:])
+        S += s + '\n'
+        S += '-'*len(s) + '\n\n'
+
+        for x in sorted(fg[k]):
+
+            q = x[3:-3].replace('/', '.')
+            t = ' '.join(q.split('.')[-1].split('_')).capitalize()
+
+            S += t + '\n'
+            S += '+'*len(t) + '\n'
+            S += '.. automodule:: {}'.format(q) + '\n'
+            S += '   :members:\n\n'
+
+    S = template.render(project_name=project_name, refs=S)
+    with open(doc_index_path, 'w') as f:
+        f.write(S)
+
+
+if __name__ == '__main__':
+    p = janus.ArgParser()
+    p.new_str('t template')
+    p.new_str('s save')
+    p.parse()
+    make_doc_index(p['template'], p['save'])
