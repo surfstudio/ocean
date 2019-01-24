492e8447d154e7751ff24a6154c68d43d65e7bc2
Alexander Olferuk <olferuk@surfstudio.ru>
2019-01-24 14:48:05 +0300

    Docs formatting

--- ocean/{{projectNameShort}}/utils/docs_index_generator.py
+++ ocean/{{projectNameShort}}/utils/docs_index_generator.py
@@ -24,7 +24,7 @@ def make_doc_index(template_path: str, doc_index_path: str):
     for k in sorted(fg.keys()):
         s = 'Module {}'.format(k[3:])
         S += s + '\n'
-        S += '-'*len(s) + '\n\n'
+        S += '='*len(s) + '\n\n'
 
         for x in sorted(fg[k]):
 
@@ -32,11 +32,13 @@ def make_doc_index(template_path: str, doc_index_path: str):
             t = ' '.join(q.split('.')[-1].split('_')).capitalize()
 
             S += t + '\n'
-            S += '+'*len(t) + '\n'
+            S += '*'*len(t) + '\n'
             S += '.. automodule:: {}'.format(q) + '\n'
-            S += '   :members:\n\n'
+            S += '   :members:\n'
+            S += '   :noindex:\n\n'
 
-    S = template.render(project_name=project_name, refs=S)
+    rendered_project_name = '{0}\n{1}'.format(project_name, '='*len(project_name))
+    S = template.render(project_name=rendered_project_name, refs=S)
     with open(doc_index_path, 'w') as f:
         f.write(S)
 
