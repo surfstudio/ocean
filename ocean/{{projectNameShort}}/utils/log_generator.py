28d622ec6835dd71b507d024edfc6a9afa6304c9
Alexandr Olferuk <olferuk@surfstudio.ru>
2019-02-08 14:41:02 +0300

    Project log generation

---
+++ ocean/{{projectNameShort}}/utils/log_generator.py
@@ -0,0 +1,208 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+from glob import glob
+from jinja2 import Template
+
+import json
+import mistune
+import os
+import re
+import shutil
+import sys
+
+
+HINTS = {
+    'Raw': 'Raw data provided by the customer',
+    'External': 'Data taken from the Internet like weather or maps',
+    'Interim': 'Intermediate data respresentation: filtered and cleaned',
+    'Features': 'Features to merge with: calculated statistics and other',
+    'Processed': 'Data ready to be put into models'
+}
+
+def parse_md(path):
+    return BeautifulSoup(mistune.Markdown()(open(path).read()), 'lxml')
+
+def extract_text(t):
+    '''preserves <code></code> in the text'''
+    return ''.join([str(x) for x in t.contents])
+
+def create_experiment(md):
+    exp = {}
+    exp['exp_name'] = extract_text(md.select_one('h1'))
+    exp['task'] = extract_text(md.find('h2', text='Task').find_next('p'))
+    exp['author'] = extract_text(md.find('h2', text='Author').find_next('p'))
+
+    rows = md.find('h3', text='Data generated')\
+             .find_next_sibling('table')\
+             .select_one('tbody')\
+             .select('tr')
+    exp['data_generated'] = []
+    for row in rows:
+        try:
+            path, comment = row.select('td')
+            clear_path = extract_text(path).strip()
+            if clear_path == '':
+                continue
+            exp['data_generated'].append({
+                'path': extract_text(clear_path),
+                'comment': extract_text(comment)
+            })
+        except ValueError:
+            continue
+
+    rows = md.find('h3', text='Data taken')\
+             .find_next_sibling('ul')\
+             .find_all('li')
+    exp['data_taken'] = [row.text for row in rows]
+
+    exp['log'] = []
+    for log_record in md.find('h2', text='Log').find_next_siblings('p'):
+        lines = extract_text(log_record).split('\n')
+        datetime = lines[0]
+        text = '\n'.join(lines[1:])
+        exp['log'].append({'datetime': datetime, 'text': text})
+
+    return exp
+
+def to_dt(s):
+    for p in ['%d.%m.%y, %H:%M',
+              '%d.%m.%y %H:%M',
+              '%d.%m.%Y, %H:%M',
+              '%d.%m.%Y %H:%M']:
+        try:
+            return datetime.strptime(s, '%d.%m.%y, %H:%M').timestamp()
+        except:
+            continue
+    return datetime.now().timestamp()
+
+def fill_experiments(total, exps):
+    for exp in exps:
+        item = {
+            'title': exp['exp_name'],
+            'task': exp['task'],
+            'log': []
+        }
+        for record in exp['log']:
+            item['log'].append({
+                'datetime': record['datetime'],
+                'text': record['text'],
+                'author': exp['author']
+            })
+        total['experiments'].append(item)
+
+def fill_history(total, exps):
+    records = []
+    for exp in exps:
+        for record in exp['log']:
+            item = {
+                'datetime': record['datetime'],
+                'text': record['text'],
+                'exp': 'Exp. {}'.format(re.findall(r'Experiment (\d+)', exp['exp_name'])[0]),
+                'author': exp['author']
+            }
+            records.append(item)
+    total['history'] = sorted(records, key=lambda d: to_dt(d['datetime']))
+
+def fill_data(total, exps):
+    for e in exps:
+        full_exp_name = e['exp_name'][len('Experiment '):]
+        exp_name = 'Exp. {}'.format(re.findall(r'Experiment (\d+)', e['exp_name'])[0])
+
+        for p in e['data_generated']:
+            item = {
+                'comment': p['comment'],
+                'filename': p['path'].split('/')[-1],
+                'created_in': exp_name,
+                'used_in': [exp_name]
+            }
+            path = p['path']
+
+            print('PATH: ', path)
+
+            if path.startswith('/'):
+                path = path[1:]
+            ps = path.split('/')
+            if path.startswith('experiments/exp-'):
+                models = total['data']['models']
+                if full_exp_name in models:
+                    models[full_exp_name].append(item)
+                else:
+                    models[full_exp_name] = [item]
+            else:
+                data = total['data']['data']
+                key = ps[1]
+                if key in data:
+                    data[key].append(item)
+                else:
+                    data[key] = [item]
+        for p in e['data_taken']:
+            if p.startswith('/'):
+                p = p[1:]
+            ps = p.split('/')
+            main_key = ps[0]
+            key = ps[1]
+            filename = ps[-1]
+            d = total['data'][main_key]
+            if key not in d:
+                d[key] = [{
+                    'comment': '-',
+                    'filename': filename,
+                    'created_in': '<i>Unknown</i>',
+                    'used_in': [exp_name]
+                }]
+            else:
+                found = False
+                for rec in d[key]:
+                    if rec['filename'] == filename:
+                        found = True
+                        rec['used_in'].append(exp_name)
+                        rec['used_in'] = sorted(set(rec['used_in']))
+                if not found:
+                    d[key].append({
+                        'comment': '-',
+                        'filename': filename,
+                        'created_in': '<i>Unknown</i>',
+                        'used_in': [exp_name]
+                    })
+
+if __name__ == '__main__':
+    utils_folder = os.path.dirname(os.path.abspath(__file__))
+    template_folder = os.path.join(utils_folder, 'project_log')
+    root_folder = os.path.dirname(utils_folder)
+    log_folder = os.path.join(root_folder, 'project_log')
+    exp_folder = os.path.join(root_folder, 'experiments/')
+    html_path = os.path.join(log_folder, 'project_log.html')
+    readme_path = os.path.join(root_folder, 'README.md')
+
+    if os.path.exists(log_folder):
+        shutil.rmtree(log_folder)
+
+    total = {
+        'experiments': [],
+        'history': [],
+        'data': {
+            'data': {},
+            'models': {}
+        },
+        'hints': HINTS
+    }
+
+    ps = sorted(glob(exp_folder + '*/*.md'))
+    if len(ps) == 0:
+        print('No logs were found')
+    else:
+        shutil.copytree(template_folder, log_folder)
+        mds = [parse_md(p) for p in ps]
+        exps = [create_experiment(md) for md in mds]
+
+        fill_experiments(total, exps)
+        fill_history(total, exps)
+        fill_data(total, exps)
+
+        tree = parse_md(readme_path)
+        project_name = tree.select_one('h1').text
+
+        s = json.dumps(total, indent=2)
+        s = Template(open(html_path).read()).render(json=s, projectName=project_name)
+        with open(html_path, 'w') as f:
+            f.write(s)
