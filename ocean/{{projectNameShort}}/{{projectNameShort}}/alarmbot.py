b21fb479f9b127aea90d69f58872fa300c8fe1e1
palushok <dpaluhin@gmail.com>
2019-02-14 13:17:28 +0300

    spaces enabled in message notification

--- ocean/{{projectNameShort}}/{{projectNameShort}}/alarmbot.py
+++ ocean/{{projectNameShort}}/{{projectNameShort}}/alarmbot.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen as get
+from urllib.parse import quote
 
 
 def notify(token, message):
-    get("https://alarmerbot.ru/?key={0}&message={1}".format(token, message))
+    get("https://alarmerbot.ru/?key={0}&message={1}".format(token, quote(message)))
